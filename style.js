921480256a8f1c3d72eb23bba551e7b10a274dbd
KainatHaq <kainathaq52@gmail.com>
2024-09-28 09:25:52 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,3 @@
+function changecolor() {
+    document.body.style.backgroundColor = (`rgb(${Math.floor(Math.random() * 235)},${Math.floor(Math.random() * 240)}, ${Math.floor(Math.random() * 255)})`)
+}
